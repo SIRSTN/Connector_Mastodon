918c951590e33fd6c49f7c2901c36ee9e05d2f31
SIRSTN <stephane.velen@gmail.com>
2023-11-23 11:20:06 +0100

    Ethereum commit

--- app.py
+++ app.py
@@ -23,8 +23,9 @@ mastodon = Mastodon(
     api_base_url=api_base_url
 )
 
-# Keywords for Mastodon search
-keywords = ["Bitcoin", "BTC"]
+# Keywords for Bitcoin and Ethereum Mastodon search
+keywords_bitcoin = ["Bitcoin", "BTC"]
+keywords_ethereum = ["Ethereum", "ETH"]
 
 # Define the timeframe of the last 12 hours
 timeframe_start = datetime.now(timezone.utc) - timedelta(hours=12)
@@ -32,78 +33,81 @@ timeframe_start = datetime.now(timezone.utc) - timedelta(hours=12)
 # API URL to send data
 api_url = "http://localhost:5000/store-text"
 
-# List to store entries to be sent to the API
-entries_to_store = []
+# Function to process Mastodon posts
+def process_mastodon_posts(toots, keywords):
+    entries_to_store = []
+    toot_number = 0
+    for toot in toots:
+        toot_date = toot['created_at']
 
-# Initialize a counter for comment numbers
-toot_number = 0
+        # Skip toots that are older than the timeframe_start
+        if toot_date < timeframe_start:
+            continue
 
-# For each keyword, perform the search and process the results
-for keyword in keywords:
-    max_id = None  # Initialize max_id for pagination
-    keep_searching = True
+        soup = BeautifulSoup(toot['content'], 'html.parser')
+        toot_content = soup.get_text()
 
-    while keep_searching:
-        # Search for the keyword in Mastodon
-        search_result = mastodon.search(q=keyword, max_id=max_id, resolve=False)
-        toots = search_result['statuses']
-        
-        if not toots:
-            break
+        toot_number += 1
+        print(f"Toot #{toot_number}:")
+        print(toot_content)
+        print()
 
-        # Loop over found toots and process them
-        for toot in toots:
-            toot_date = toot['created_at']
+        entry = {
+            'user': toot['account']['username'],
+            'text': toot_content,
+            'date': toot_date.isoformat()
+        }
 
-            # If the toot is older than 12 hours, stop processing
-            if toot_date < timeframe_start:
-                keep_searching = False
+        entries_to_store.append(entry)
+    
+    return entries_to_store
+
+# Function to search and process toots
+def search_mastodon(keywords):
+    entries_to_store = []
+    for keyword in keywords:
+        max_id = None
+        keep_searching = True
+
+        while keep_searching:
+            search_result = mastodon.search(q=keyword, max_id=max_id, resolve=False)
+            toots = search_result['statuses']
+
+            if not toots:
                 break
 
-            # Use BeautifulSoup to extract text (if you want to strip HTML tags)
-            soup = BeautifulSoup(toot['content'], 'html.parser')
-            toot_content = soup.get_text()
+            processed_entries = process_mastodon_posts(toots, keywords)
+            entries_to_store.extend(processed_entries)
 
-            # Print the comment number and the toot content
-            toot_number += 1
-            print(f"Toot #{toot_number}:")
-            print(toot_content)
-            print()
+            if not keep_searching or len(toots) < 40:
+                break
 
-            # Create the entry for each toot
-            entry = {
-                'user': toot['account']['username'],
-                'text': toot_content,
-                'date': toot_date.isoformat()  # No need to convert as it's already a datetime object
-            }
+            max_id = toots[-1]['id']
+            time.sleep(1)
+    
+    return entries_to_store
 
-            entries_to_store.append(entry)
-
-        # If we have processed all toots or found an old toot, stop searching
-        if not keep_searching or len(toots) < 40:  # Assuming default Mastodon API pagination size
-            break
-
-        # Set max_id to the ID of the last toot for the next pagination batch
-        max_id = toots[-1]['id']
-
-        # Avoid hitting the rate limit
-        time.sleep(1)
-
-# Send the collected toot data to the local API
-if entries_to_store:
-    # Prepare the data for the API request
-    data_to_send = {
-        'source': 'Mastodon',
-        'keyword': 'Bitcoin',
-        'entries': entries_to_store
-    }
-
-    # Send a POST request to the API with the toot data
-    response = requests.post(api_url, json=data_to_send)
-    print("Status Code:", response.status_code)
-    if response.status_code == 200:
-        print("Response:", response.json())
+# Function to send data to API
+def send_to_api(entries, source, keyword):
+    if entries:
+        data = {
+            'source': source,
+            'keyword': keyword,
+            'entries': entries
+        }
+        response = requests.post(api_url, json=data)
+        print(f"{source} - {keyword} Search - Status Code:", response.status_code)
+        if response.status_code == 200:
+            print(f"{source} - {keyword} Search - Response:", response.json())
+        else:
+            print(f"{source} - {keyword} Search - Error response:", response.text)
     else:
-        print("Error response:", response.text)
-else:
-    print("No new toots found in the last 12 hours.")
+        print(f"No new {keyword} posts found from {source} in the last 12 hours.")
+
+# Search for Bitcoin related toots and send them to the API
+btc_entries = search_mastodon(keywords_bitcoin)
+send_to_api(btc_entries, 'Mastodon', 'Bitcoin')
+
+# Search for Ethereum related toots and send them to the API
+eth_entries = search_mastodon(keywords_ethereum)
+send_to_api(eth_entries, 'Mastodon', 'Ethereum')
